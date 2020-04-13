52a66fdef1741af8ce10536bc5785c03391a22aa
dnelson1747 <dnelson1747@gmail.com>
2020-04-13 11:23:31 -0700

    Add project files.

---
+++ BlazorApp1/Data/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class TaskModel
+    {
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
