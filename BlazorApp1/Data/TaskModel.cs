63d54a06fdf429e228afd738a67231150862c79a
David Nelson <dnelson1747@gmail.com>
2021-04-13 18:45:06 -0700

    Car Maintenance app

--- BlazorApp1/Data/TaskModel.cs
+++ BlazorApp1/Data/TaskModel.cs
@@ -8,6 +8,9 @@ namespace BlazorApp1.Data
     public class TaskModel
     {
         public string Task { get; set; }
+        //public bool IsStarted Task { get; set; }
+        //public bool InProgress { get; set; }
+        
         public bool IsComplete { get; set; }
     }
 }
--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -16,6 +16,7 @@ namespace BlazorApp1
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+           
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
