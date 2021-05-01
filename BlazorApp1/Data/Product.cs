dbec957def88ab017de05f7d08ef6a38aa6b3562
David Nelson <dnelson1747@gmail.com>
2021-05-01 11:59:37 -0700

    Add data grid

---
+++ BlazorApp1/Data/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class Product
+    {
+        public int ProdID { get; set; }
+        public string ProdName { get; set; }
+        public double SalesPrice { get; set; }
+        public DateTime PurchaseDate { get; set; }
+
+        public double Price { get; set; }
+    }
+}
---
+++ BlazorApp1/Data/ProductService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp1.Data
+{
+    public class ProductService
+    {
+        List<Product> products = new()
+        {
+            new Product() { ProdID = 1, ProdName = "Car Wash", SalesPrice = 10, PurchaseDate = Convert.ToDateTime("01-May-2021") },
+            new Product() { ProdID = 2, ProdName = "Gas", SalesPrice = 3, PurchaseDate = Convert.ToDateTime("01-May-2021") },
+            new Product() { ProdID = 3, ProdName = "Tune Up", SalesPrice = 15, PurchaseDate = Convert.ToDateTime("01-May-2021") },
+            new Product() { ProdID = 4, ProdName = "Vacuum", SalesPrice = 5, PurchaseDate = Convert.ToDateTime("01-May-2021") },
+            new Product() { ProdID = 5, ProdName = "Spark Plugs", SalesPrice = 8, PurchaseDate = Convert.ToDateTime("01-May-2021") },
+            new Product() { ProdID = 6, ProdName = "Oil Change", SalesPrice = 12, PurchaseDate = Convert.ToDateTime("01-May-2021") }
+
+        };
+
+        
+        public async Task<List<Product>> ProductList()
+        {
+            return await Task.FromResult(products);
+        }
+
+
+    }
+}
